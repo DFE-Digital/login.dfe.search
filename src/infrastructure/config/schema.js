ce490e0f15520d87cbeee5ba709c7d225fa45906
Simon Bull <simon.bull.84@googlemail.com>
2018-10-26 12:03:44 +0100

    Update config schema

--- src/infrastructure/config/schema.js
+++ src/infrastructure/config/schema.js
@@ -1,5 +1,5 @@
 const SimpleSchema = require('simpl-schema').default;
-const { validateConfigAgainstSchema, schemas } = require('login.dfe.config.schema.common');
+const { validateConfigAgainstSchema, schemas, patterns } = require('login.dfe.config.schema.common');
 const config = require('./index');
 const logger = require('./../logger');
 
@@ -7,11 +7,45 @@ const schedulesTasksSchema = new SimpleSchema({
   reindexUsers: String,
   updateAuditCache: String,
 });
+const cacheSchema = new SimpleSchema({
+  type: {
+    type: String,
+    allowedValues: ['memory', 'redis'],
+  },
+  params: {
+    type: Object,
+    optional: true,
+    custom: function() {
+      if (this.siblingField('type').value === 'redis' && !this.isSet) {
+        return SimpleSchema.ErrorTypes.REQUIRED
+      }
+    },
+  },
+  'params.connectionString': {
+    type: String,
+    regEx: patterns.redis,
+    optional: true,
+    custom: function() {
+      if (this.field('type').value === 'redis' && !this.isSet) {
+        return SimpleSchema.ErrorTypes.REQUIRED
+      }
+    },
+  },
+});
+const auditSchema = new SimpleSchema({
+  type: {
+    type: String,
+    allowedValues: ['sequelize'],
+  },
+  params: schemas.sequelizeConnection,
+});
 
 const schema = new SimpleSchema({
   loggerSettings: schemas.loggerSettings,
   hostingEnvironment: schemas.hostingEnvironment,
   schedulesTasks: schedulesTasksSchema,
+  cache: cacheSchema,
+  audit: auditSchema,
 });
 module.exports.validate = () => {
   validateConfigAgainstSchema(config, schema, logger)
