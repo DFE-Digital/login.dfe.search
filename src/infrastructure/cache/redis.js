0a7f30677247f2d914574c965d3a20838ac87512
Simon Bull <simon.bull.84@googlemail.com>
2018-10-26 11:57:09 +0100

    Implement redis cache

--- src/infrastructure/cache/redis.js
+++ src/infrastructure/cache/redis.js
@@ -1,9 +1,19 @@
-const get = async (key) => {
+const Redis = require('ioredis');
+const config = require('./../config');
 
+const redis = new Redis(config.cache.params.connectionString);
+
+const get = async (key) => {
+  const value = await redis.get(key);
+  return value ? JSON.parse(value) : undefined;
 };
 
 const set = async (key, value, secondsToLive = 0) => {
-
+  if(secondsToLive>0){
+    await redis.multi().set(key, JSON.stringify(value)).expire(secondsToLive).exec();
+  } else {
+    await redis.set(key, JSON.stringify(value));
+  }
 };
 
 module.exports = {
