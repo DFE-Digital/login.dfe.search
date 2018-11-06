8e81717d5dff47c268d490ccda07ca2bd9e2b540
Simon Bull <simon.bull.84@googlemail.com>
2018-11-06 08:38:36 +0000

    Add get user by id endpoint

--- src/app/users/index.js
+++ src/app/users/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const { asyncWrapper } = require('login.dfe.express-error-handling');
 
 const search = require('./search');
+const getById = require('./getById');
 
 const router = express.Router({ mergeParams: true });
 
 const area = () => {
   router.get('/', asyncWrapper(search));
+  router.get('/:uid', asyncWrapper(getById));
 
   return router;
 };
