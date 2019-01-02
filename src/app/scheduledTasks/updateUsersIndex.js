48149bb237389d7bcee4ae428fd365ae418eb342
Simon Bull <simon.bull.84@googlemail.com>
2019-01-02 09:56:22 +0000

    Don't attempt user diff if no current index

--- src/app/scheduledTasks/updateUsersIndex.js
+++ src/app/scheduledTasks/updateUsersIndex.js
@@ -3,16 +3,21 @@ const UserIndex = require('./../indexes/UserIndex');
 const cache = require('./../../infrastructure/cache');
 
 const updateUsersIndex = async (correlationId) => {
+  const index = await UserIndex.current();
+  if (!index) {
+    logger.warn(`Abandoning update user index as no current index set.`, { correlationId });
+    return;
+  }
+
   const lastUpdated = new Date(await cache.get('Pointer:LastUserUpdateTime') || 0);
   const start = new Date();
 
-  const index = await UserIndex.current();
-  logger.info(`Starting to process users updated since ${lastUpdated} into ${index.name}`);
+  logger.info(`Starting to process users updated since ${lastUpdated} into ${index.name}`, { correlationId });
 
   await index.indexUsersChangedAfter(lastUpdated, correlationId);
-  logger.info('Finished processing users updated since ${lastUpdated} into ${index.name}', {correlationId});
+  logger.info('Finished processing users updated since ${lastUpdated} into ${index.name}', { correlationId });
 
   await cache.set('Pointer:LastUserUpdateTime', start.getTime());
-  logger.info(`Updated user update pointer to ${start}`);
+  logger.info(`Updated user update pointer to ${start}`, { correlationId });
 };
 module.exports = updateUsersIndex;
