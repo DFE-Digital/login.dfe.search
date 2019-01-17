1f1e51da8a8871842b5b1febdde20e84326abac7
Simon Bull <simon.bull.84@googlemail.com>
2019-01-17 21:17:23 +0000

    Tidy devices

--- src/app/scheduledTasks/tidyIndexes.js
+++ src/app/scheduledTasks/tidyIndexes.js
@@ -1,9 +1,12 @@
 const logger = require('./../../infrastructure/logger');
 const UserIndex = require('./../indexes/UserIndex');
-
+const DeviceIndex = require('./../indexes/DeviceIndex');
 
 const tidyIndexes = async (correlationId) => {
   logger.info('tidying user indexes', { correlationId });
-  await UserIndex.tidyIndexes(correlationId)
+  await UserIndex.tidyIndexes(correlationId);
+
+  logger.info('tidying device indexes', { correlationId });
+  await DeviceIndex.tidyIndexes(correlationId);
 };
 module.exports = tidyIndexes;
