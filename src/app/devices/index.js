91087ae43255d927b0fe03241fab58b8df080823
Simon Bull <simon.bull.84@googlemail.com>
2019-01-18 14:31:18 +0000

    Get by device by serial number endpoint

--- src/app/devices/index.js
+++ src/app/devices/index.js
@@ -2,14 +2,14 @@ const express = require('express');
 const { asyncWrapper } = require('login.dfe.express-error-handling');
 
 const search = require('./search');
-// const getById = require('./getById');
+const getBySerialNumber = require('./getBySerialNumber');
 // const update = require('./update');
 
 const router = express.Router({ mergeParams: true });
 
 const area = () => {
   router.get('/', asyncWrapper(search));
-  // router.get('/:uid', asyncWrapper(getById));
+  router.get('/:sn', asyncWrapper(getBySerialNumber));
   // router.patch('/:uid', asyncWrapper(update));
 
   return router;
