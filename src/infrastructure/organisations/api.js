0c7055c00697b29714f7f3d1a97333d723a06a7e
Shivanshu Patel <shivanshu.patel@education.gov.uk>
2021-09-02 11:41:38 +0100

    Replace list users organisation api with DAO method

--- src/infrastructure/organisations/api.js
+++ src/infrastructure/organisations/api.js
@@ -1,12 +1,11 @@
 const config = require('./../config');
 const ApiClient = require('./../utils/ApiClient');
-const { invitation } = require('login.dfe.dao');
+const { invitation, organisation } = require('login.dfe.dao');
 
 const client = new ApiClient(config.organisations.service);
 
-const listUsersOrganisations = async (page, pageSize, correlationId) => {
-  return client.get(`/organisations/users?page=${page}&pageSize=${pageSize}`, correlationId);
-};
+const listUsersOrganisations = async (page, pageSize) => organisation.getUsersOrganisations(page, pageSize);
+
 const getUserOrganisations = async (userId, correlationId) => {
   return client.get(`/organisations/associated-with-user/${userId}`, correlationId);
 };
