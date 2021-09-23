d55ffa18e2b9db4b97c6bddbd0e9b9891ccf1683
Jose Luis Garcia Ramos <joseluisgraa@gmail.com>
2021-09-23 12:53:01 +0100

    Fixed search with criteria, re-added GET

--- src/app/users/index.js
+++ src/app/users/index.js
@@ -10,6 +10,7 @@ const deleteUser = require('./delete');
 const router = express.Router({ mergeParams: true });
 
 const area = () => {
+  router.get('/', asyncWrapper(search));
   router.post('/', asyncWrapper(search));
   router.post('/update-index', asyncWrapper(create));
   router.get('/:uid', asyncWrapper(getById));
