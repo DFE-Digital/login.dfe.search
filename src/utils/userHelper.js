bf95d6579a589df4052aaad8b36c08a4b3eff022
Jared Marsh <jared.marsh@education.gov.uk>
2023-03-06 10:09:35 +0000

    Added overwriteAuditLastLogin function
    Overwrites audit last login for a user with value from the dirs DB.

---
+++ src/utils/userHelper.js
@@ -0,0 +1,18 @@
+const { getUser } = require('../infrastructure/directories');
+
+/**
+ * Overwrite the lastLogin property in a user object from UserIndex
+ * to the value from the user table (last_login) in the directories database.
+ * @param {Object} userData - User data retrieved from the UserIndex.
+ * @returns {Object} - User data object with the last login updated from the database.
+ */
+async function overwriteAuditLastLogin(userData) {
+  const user = userData;
+  const databaseUser = await getUser(user.id);
+  user.lastLogin = (databaseUser.last_login);
+  return user;
+}
+
+module.exports = {
+  overwriteAuditLastLogin,
+};
