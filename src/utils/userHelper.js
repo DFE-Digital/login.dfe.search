3dacd0a4fd28f0c79252ab5addf16854882d5cc9
Jared Marsh <jared.marsh@education.gov.uk>
2023-03-06 16:25:05 +0000

    Updated overwriteAuditLastLogin return type

--- src/utils/userHelper.js
+++ src/utils/userHelper.js
@@ -4,7 +4,7 @@ const { getUser } = require('../infrastructure/directories');
  * Overwrite the lastLogin property in a user object from UserIndex
  * to the value from the user table (last_login) in the directories database.
  * @param {Object} userData - User data retrieved from the UserIndex.
- * @returns {Object} - User data object with the last login updated from the database.
+ * @returns {Promise<Object>} - User data object with the last login updated from the database.
  */
 async function overwriteAuditLastLogin(userData) {
   const user = userData;
