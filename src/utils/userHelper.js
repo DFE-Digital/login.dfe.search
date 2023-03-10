33c6ec429f68edd998a2432b14a14b22842abf26
Jared Marsh <jared.marsh@education.gov.uk>
2023-03-10 11:07:27 +0000

    Added overwriteAuditLastLogins function
    Overwrites audit last login for multiple users with their dirs DB value.

--- src/utils/userHelper.js
+++ src/utils/userHelper.js
@@ -1,4 +1,4 @@
-const { getUser } = require('../infrastructure/directories');
+const { getUser, getUsers } = require('../infrastructure/directories');
 
 /**
  * Overwrite the lastLogin property in a user object from UserIndex
@@ -13,6 +13,31 @@ async function overwriteAuditLastLogin(userData) {
   return user;
 }
 
+/**
+ * Overwrite the lastLogin property in multiple user objects from UserIndex
+ * to their values from the user table (last_login) in the directories database.
+ * @param {Object[]} usersData Multiple user's data retrieved from the UserIndex.
+ * @returns {Promise<Object[]>} Multiple users data object with the last login updated from the database.
+ */
+async function overwriteAuditLastLogins(usersData) {
+  const users = usersData;
+  const userIndexes = new Map(users.map((user, index) => [user.id, index]));
+  // Invitations have a different ID, and they won't have a last login, so cannot be overwritten.
+  const userOverwriteIds = usersData
+    .filter((user) => !user.id.startsWith('inv-'))
+    .map((user) => user.id);
+
+  if (userOverwriteIds.length > 0) {
+    const databaseUsers = await getUsers(userOverwriteIds);
+    databaseUsers.forEach((dbUser) => {
+      users[userIndexes.get(dbUser.sub)].lastLogin = dbUser.last_login;
+    });
+  }
+
+  return users;
+}
+
 module.exports = {
   overwriteAuditLastLogin,
+  overwriteAuditLastLogins,
 };
