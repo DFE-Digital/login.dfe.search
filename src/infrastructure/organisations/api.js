ad89ef63f57c31ceb2f0b21ed7a69d4e0fba8180
Jared Marsh <jared.marsh@education.gov.uk>
2024-08-13 16:42:00 +0100

    Remove unused API calls from infrastructure

--- src/infrastructure/organisations/api.js
+++ src/infrastructure/organisations/api.js
@@ -1,31 +1,18 @@
 const config = require('./../config');
 const ApiClient = require('./../utils/ApiClient');
-const { invitation, organisation } = require('login.dfe.dao');
+const { invitation } = require('login.dfe.dao');
 
 const client = new ApiClient(config.organisations.service);
-
-const listUsersOrganisations = async (page, pageSize) => organisation.getUsersOrganisations(page, pageSize);
-
-const getUserOrganisations = async (userId, correlationId) => {
-  return client.get(`/organisations/associated-with-user/${userId}`, correlationId);
-};
 
 const getUserOrganisationsV2 = async (userId, correlationId) => {
   return client.get(`/organisations/v2/associated-with-user/${userId}`, correlationId);
 }
-
-const listInvitationsOrganisations = async (page, pageSize) => {
-  return invitation.listInvitationsForOrganisations(page, pageSize);
-};
 
 const getInvitationOrganisations = async (invitationId) => {
   return invitation.getInvitationResponseById(invitationId);
 }
 
 module.exports = {
-  listUsersOrganisations,
-  getUserOrganisations,
-  listInvitationsOrganisations,
   getInvitationOrganisations,
   getUserOrganisationsV2,
 };
--- src/infrastructure/organisations/static.js
+++ src/infrastructure/organisations/static.js
@@ -1,33 +1,12 @@
-const listUsersOrganisations = async (page, pageSize) => {
-  return Promise.resolve({
-    userOrganisations: [],
-    totalNumberOfRecords: 0,
-    totalNumberOfPages: 0,
-  });
-};
-const getUserOrganisations = async (userId, correlationId) => {
-  return Promise.resolve([]);
-};
-
 const getUserOrganisationsV2 = async (userId, correlationId) => {
   return Promise.resolve([]);
 };
 
-const listInvitationsOrganisations = async (page, pageSize) => {
-  return Promise.resolve({
-    invitationOrganisations: [],
-    totalNumberOfRecords: 0,
-    totalNumberOfPages: 0,
-  });
-};
 const getInvitationOrganisations = async (invitationId) => {
   return Promise.resolve([]);
 };
 
 module.exports = {
-  listUsersOrganisations,
-  getUserOrganisations,
-  listInvitationsOrganisations,
   getInvitationOrganisations,
   getUserOrganisationsV2,
 };
