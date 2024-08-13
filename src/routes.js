edbbd3d2901e6b2dcfcef6aef0b3628a28453091
Jared Marsh <jared.marsh@education.gov.uk>
2024-08-13 12:50:52 +0100

    Remove dev UI and tool scripts designed to manually run scheduled tasks

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,7 @@
 const users = require('./app/users');
-//const devices = require('./app/devices');
-const devTaskHelpers = require('./app/devTaskHelpers');
-
-const config = require('./infrastructure/config');
-
 
 const registerRoutes = (app) => {
   app.use('/users', users());
-  //app.use('/sch/devices', devices());
-
-  if (config.hostingEnvironment.useDevViews) {
-    app.use('/', devTaskHelpers());
-  }
 };
 
 module.exports = registerRoutes;
