422ddd9041bdd7bb66595362e8d242e18aecbf35
Univer-lion-lab <rafael.tsiakkouris@methods.co.uk>
2025-02-06 10:18:45 +0000

    Revert "Eslint fixes"
    This reverts commit 6073a0f172b681963e9f88bbf4b1ff3ca7887f00.

--- src/infrastructure/search/azureSearch.js
+++ src/infrastructure/search/azureSearch.js
@@ -1,5 +1,5 @@
 const asyncRetry = require("login.dfe.async-retry");
-//const { fetchApi } = require("login.dfe.async-retry");
+const { fetchApi } = require("login.dfe.async-retry");
 const { apiStrategy } = require("login.dfe.async-retry/lib/strategies");
 const omit = require("lodash/omit");
 const config = require("../config");
@@ -8,11 +8,11 @@ const {
 } = require("../../utils/userSearchHelpers");
 //Poc work
 const { DefaultAzureCredential } = require("@azure/identity");
-const { SearchClient } = require("@azure/search-documents");
+const { SearchClient, SearchIndexClient } = require("@azure/search-documents");
 const credential = new DefaultAzureCredential();
 
-// const baseUri = `https://${config.search.azureSearch.serviceName}.search.windows.net/indexes`;
-// const apiVersion = "2020-06-30";
+const baseUri = `https://${config.search.azureSearch.serviceName}.search.windows.net/indexes`;
+const apiVersion = "2020-06-30";
 
 const azureSearchClient = (indexName) => {
   return new SearchClient(`https://${config.cache.params.serviceName}.search.windows.net`, indexName ,credential);
@@ -37,7 +37,7 @@ const storeDocumentsInIndex = async (name, documents) =>
     //     `Request failed with status code ${response.status} (POST: ${url})`,
     //   );
     // }
-    const response = await azureSearchClient(name).uploadDocuments([documents])
+    response = await azureSearchClient(name).uploadDocuments([documents])
 
     return {
       statusCode: response._response.status,
@@ -51,7 +51,7 @@ const deleteDocumentInIndex = async (name, id) => {
     id: id // Replace with the ID of the document you want to delete
   }
 
-  await azureSearchClient(name).deleteDocuments([document])
+  response = await azureSearchClient(name).deleteDocuments([document])
   
   // await fetchApi(`${baseUri}/${name}/docs/index?api-version=${apiVersion}`, {
   //   method: "POST",
@@ -82,12 +82,12 @@ const searchIndex = async (
 ) => {
   const skip = (page - 1) * pageSize;
   //let uri = `${baseUri}/${name}/docs?api-version=${apiVersion}&search=${criteria}&$count=true&$skip=${skip}&$top=${pageSize}&queryType=full&searchMode=all`;
-  //let orderBy = ""
+  let orderBy = ""
   let filterParam = "";
-  // if (sortBy) {
-  //    orderBy = sortAsc ? sortBy : `${sortBy} desc`;
-  //   //uri += `&$orderby=${orderBy}`;
-  // }
+  if (sortBy) {
+     orderBy = sortAsc ? sortBy : `${sortBy} desc`;
+    //uri += `&$orderby=${orderBy}`;
+  }
 
   // if (searchFields) {
   //   uri += `&searchFields=${searchFields}`;
@@ -121,7 +121,7 @@ const searchIndex = async (
 
     
   }
-  const response = await azureSearchClient(name).search(searchText, options)
+  response = await azureSearchClient(name).search(searchText, options)
 
   // const response = await fetchApi(uri, {
   //   method: "GET",
