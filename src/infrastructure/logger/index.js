782f3f51c42d62303bd6c5f9ab2a918a6392ed57
Ionut Predoi <ionut.predoi@education.gov.uk>
2025-05-09 11:53:42 +0100

    Add package `login.dfe.api-client` and utilise its logger
    Test Suites: 4 passed, 4 total
    Tests:       21 passed, 21 total
    Snapshots:   0 total
    Time:        3.845 s

--- src/infrastructure/logger/index.js
+++ src/infrastructure/logger/index.js
@@ -1,89 +1,23 @@
-const winston = require("winston");
-const appInsights = require("applicationinsights");
-const AuditTransporter = require("login.dfe.audit.transporter");
-const AppInsightsTransport = require("login.dfe.winston-appinsights");
+const WinstonSequelizeTransport = require("login.dfe.audit.winston-sequelize-transport");
+const {
+  setupApplicationInsights,
+  setupLogging,
+} = require("login.dfe.api-client/logging");
 const config = require("../config");
 
-const logLevel =
-  config && config.loggerSettings && config.loggerSettings.logLevel
-    ? config.loggerSettings.logLevel
-    : "info";
+const additionalTransports = [];
 
-const customLevels = {
-  levels: {
-    audit: 0,
-    error: 1,
-    warn: 2,
-    info: 3,
-    verbose: 4,
-    debug: 5,
-    silly: 6,
-  },
-  colors: {
-    info: "yellow",
-    ok: "green",
-    error: "red",
-    audit: "magenta",
-  },
-};
-
-// Formatter to hide audit records from other loggers.
-const hideAudit = winston.format((info) =>
-  info.level.toLowerCase() === "audit" ? false : info,
-);
-
-const loggerConfig = {
-  levels: customLevels.levels,
-  transports: [],
-};
-
-loggerConfig.transports.push(
-  new winston.transports.Console({
-    format: winston.format.combine(hideAudit(), winston.format.simple()),
-    level: logLevel,
-  }),
-);
-
-const opts = {
-  application: config.loggerSettings.applicationName,
-  level: "audit",
-};
-const auditTransport = AuditTransporter(opts);
-
-if (auditTransport) {
-  loggerConfig.transports.push(auditTransport);
+const sequelizeTransport = WinstonSequelizeTransport(config);
+if (sequelizeTransport) {
+  additionalTransports.push(sequelizeTransport);
 }
 
 if (config.hostingEnvironment.applicationInsights) {
-  appInsights
-    .setup(config.hostingEnvironment.applicationInsights)
-    .setAutoCollectConsole(false, false)
-    .start();
-  loggerConfig.transports.push(
-    new AppInsightsTransport({
-      format: winston.format.combine(hideAudit(), winston.format.json()),
-      client: appInsights.defaultClient,
-      applicationName: config.loggerSettings.applicationName || "Search",
-      type: "event",
-      treatErrorsAsExceptions: true,
-    }),
-  );
+  setupApplicationInsights(config.hostingEnvironment.applicationInsights);
 }
 
-const logger = winston.createLogger(loggerConfig);
-
-process.on("unhandledRejection", (reason, p) => {
-  logger.error("Unhandled Rejection at:", p, "reason:", reason);
+module.exports = setupLogging({
+  applicationName: config.loggerSettings.applicationName || "Search",
+  logLevel: config.loggerSettings?.logLevel,
+  additionalTransports,
 });
-process.on("uncaughtException", (err) => {
-  try {
-    logger.error(err.message);
-  } catch (e) {
-    console.error(`Failed to log fatal error to logger (${e.message})`);
-    console.error(err.message);
-  }
-
-  process.exit(-99);
-});
-
-module.exports = logger;
