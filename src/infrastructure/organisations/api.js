12db21e12f7f1047a3fae2b57d18c72f1969064c
Ionut Predoi <ionut.predoi@education.gov.uk>
2025-05-12 12:40:53 +0100

    Replace `getUserOrganisationsV2` with `ApiClient.getUserOrganisationsRaw`

--- src/infrastructure/organisations/api.js
+++ src/infrastructure/organisations/api.js
@@ -1,15 +1,4 @@
-const config = require("./../config");
-const ApiClient = require("./../utils/ApiClient");
 const { invitation } = require("login.dfe.dao");
-
-const client = new ApiClient(config.organisations.service);
-
-const getUserOrganisationsV2 = async (userId, correlationId) => {
-  return client.get(
-    `/organisations/v2/associated-with-user/${userId}`,
-    correlationId,
-  );
-};
 
 const getInvitationOrganisations = async (invitationId) => {
   return invitation.getInvitationResponseById(invitationId);
@@ -17,5 +6,4 @@ const getInvitationOrganisations = async (invitationId) => {
 
 module.exports = {
   getInvitationOrganisations,
-  getUserOrganisationsV2,
 };
