5fb50b948934b2cc2e5a2550cf69e69514634b97
Ionut Predoi <ionut.predoi@education.gov.uk>
2025-05-13 12:51:42 +0100

    Replace `listUserServices` with `ApiClient.getUserServicesRaw`
    Test Suites: 4 passed, 4 total
    Tests:       21 passed, 21 total
    Snapshots:   0 total
    Time:        4.743 s

--- src/infrastructure/access/api.js
+++ src/infrastructure/access/api.js
@@ -3,15 +3,10 @@ const ApiClient = require("./../utils/ApiClient");
 
 const client = new ApiClient(config.access.service);
 
-const listUserServices = async (userId, correlationId) => {
-  return client.get(`/users/${userId}/services`, correlationId);
-};
-
 const listInvitationServices = async (invitationId, correlationId) => {
   return client.get(`/invitations/${invitationId}/services`, correlationId);
 };
 
 module.exports = {
-  listUserServices,
   listInvitationServices,
 };
--- src/infrastructure/access/static.js
+++ src/infrastructure/access/static.js
@@ -1,12 +1,7 @@
-const listUserServices = async () => {
-  return Promise.resolve([]);
-};
-
 const listInvitationServices = async () => {
   return Promise.resolve([]);
 };
 
 module.exports = {
-  listUserServices,
   listInvitationServices,
 };
